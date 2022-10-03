9402bc6864386e4d97bc298494e621431dc64535
elevenhsoft <114903054+elevenhsoft@users.noreply.github.com>
2022-10-03 16:51:57 +0200

    added nord theme color palette

--- config.def.h
+++ config.def.h
@@ -15,11 +15,11 @@ static const int topbar             = 1;     /* 0 means bottom bar */
 static const int user_bh	    = 2;
 static const char *fonts[]          = { "Roboto:size=10:antialias=true:hinting=true", "JoyPixels:size=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#2E3440";
+static const char col_gray2[]       = "#3B4252";
+static const char col_gray3[]       = "#D8DEE9";
+static const char col_gray4[]       = "#E5E9F0";
+static const char col_cyan[]        = "#5E81AC";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
